413a9ef436941d1aee57d587d3e54ae074e1a6da
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2023-12-28 16:21:38 +0100

    Slide: Create a Skeleton main.rs - 16:19

---
+++ src/db.rs
@@ -0,0 +1,6 @@
+//         ╔═══════════════════════════════════════════════════════════╗
+//         ║     Provides SQLX integration for the test database.      ║
+//         ║                                                           ║
+//         ║ The database is assumed to be in-memory, and rebuilt from ║
+//         ║                 scratch on each start-up.                 ║
+//         ╚═══════════════════════════════════════════════════════════╝
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+//                   ╔═══════════════════════════════════════╗
+//                   ║ YOUTUBE - AXUM / CRUD / SQLX / SQLITE ║
+//                   ╚═══════════════════════════════════════╝
+
+mod db;
+
+use anyhow::Result;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    // Load environment variables from .env if available
+    dotenv::dotenv().ok();
+
+    // Initialize the database and obtain the connection pool
+    let connection_pool = init_db().await?;
+
+    Ok(())
 }
